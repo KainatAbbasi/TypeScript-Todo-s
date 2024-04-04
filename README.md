c8605398b21a014e3f003fd8b2a59d52c18fd37b
Kainat Abbasi <KainatAkmal2003@gmail.com>
2024-04-04 13:11:23 -0700

    Added CLI Todo's List

---
+++ README.md
@@ -0,0 +1,29 @@
+# Todo List App<br>
+<br>
+Welcome to the Todo List App! This simple Node.js application allows you to manage your tasks efficiently.<br>
+<br>
+## Features<br>
+- Add tasks to your todo list<br>
+- Delete tasks from your todo list<br>
+- View your current todo list<br>
+- Exit the application when you're done managing tasks<br>
+<br>
+## Getting Started<br>
+1. Clone this repository to your local machine.<br>
+2. Open your terminal and navigate to the project directory.<br>
+3. Run `npm install` to install the necessary dependencies.<br>
+4. Start the application by running `npm start` or `node main.js`.<br>
+<br>
+## Usage<br>
+- Upon running the application, you'll be prompted to enter your desired tasks, separated by commas.<br>
+- You can then choose to add, delete, view, or exit the application using the interactive menu.<br>
+- To add a task, select "Add" and enter the task you want to add.<br>
+- To delete a task, select "Delete" and choose the task you want to remove from the list.<br>
+- To view your current todo list, select "View".<br>
+- To exit the application, select "Exit".<br>
+<br>
+## Technologies Used<br>
+- Node.js<br>
+- Inquirer.js for interactive CLI prompts<br>
+- Chalk.js for colorful terminal output<br>
+<br>
---
+++ main.ts
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+let todo: string[] = [];
+
+console.log(chalk.green('Welcome to the Todo List!'));
+
+(async () => {
+    let todoInput = await inquirer.prompt({
+        type: 'input',
+        name: 'tasks',
+        message: chalk.blue('Enter your desired tasks which you want to perform (comma-separated):')
+    });
+
+    todo = todoInput.tasks.split(',').map((task: string) => task.trim());
+    console.log('This is your Todo list :',chalk.magenta(todoInput.tasks));
+    
+    while (true) {
+        let action = await inquirer.prompt({
+            type: 'list',
+            name: 'action',
+            message: chalk.blue('Do you want to make some Changes in your Todo list ?'),
+            choices: ['Add', 'Delete', 'View', 'Exit']
+        });
+
+        if (action.action === 'Add') {
+            let newTask = await inquirer.prompt({
+                type: 'input',
+                name: 'task',
+                message: chalk.yellow('Enter the task you want to add:')
+            });
+            todo.push(newTask.task);
+        } else if (action.action === 'Delete') {
+            let taskToDelete = await inquirer.prompt({
+                type: 'list',
+                name: 'task',
+                message: chalk.yellow('Choose a task to delete:'),
+                choices: todo
+            });
+            todo = todo.filter(task => task !== taskToDelete.task);
+        } else if (action.action === 'View') {
+            console.log(chalk.yellow('Your Todo List:'));
+            console.log(todo.join('\n'));
+        } else {
+            console.log(chalk.blue('Thank you for using the Todo List!'));
+            break;
+        }
+    }
+})();
